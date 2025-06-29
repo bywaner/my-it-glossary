349da4cbe7946cb3fff58f54eba6a6256d830a78
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 19:09:15 +1000

    Create w.js

---
+++ w.js
@@ -0,0 +1,63 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Web Application",
+    definition: "A web application is a software run from a web server. Clients can access the application using a web browser."
+  },
+  {
+    term: "Web Server",
+    definition: "Web servers host websites."
+  },
+  {
+    term: "Webcam",
+    definition: "A webcam is a video device that inputs video for viewing on an output device."
+  },
+  {
+    term: "Wide Area Network (WAN)",
+    definition: "A WAN network includes more than one physical site and often uses multiple protocols via phone lines, fiber optic cables, and satellite links."
+  },
+  {
+    term: "Wi-Fi Protected Access (WPA)",
+    definition: "WPA is an improved encryption technique for protecting Wi-Fi communications, designed to replace WEP. WPA was subsequently updated to WPA2."
+  },
+  {
+    term: "Wi-Fi Protected Setup (WPS)",
+    definition: "When using WPS to configure a network connection, users push a button on the network access point, usually a router, and another button or on-screen selection to connect the devices to associate the two devices. Some WPS-enabled devices use a PIN instead of a push-button configuration."
+  },
+  {
+    term: "Windows",
+    definition: "Windows is the general-purpose operating system from Microsoft. Windows started as version 3.1 for 16-bit computers. Microsoft operating systems evolved to support servers, desktops, gaming consoles, laptops, tablets, and mobile devices."
+  },
+  {
+    term: "Windows Defender",
+    definition: "Windows Defender is an anti-spyware tool provided with MS Windows Vista, MS Windows 7, and MS Windows 8. At the time of this writing, Windows AntiSpyware is in beta release."
+  },
+  {
+    term: "Windows Explorer",
+    definition: "Windows Explorer opens as a window and performs as the standard interface for managing files and folders. In MS Windows 10, the capability is called File Explorer."
+  },
+  {
+    term: "Windows Firewall",
+    definition: "Windows Firewall is a default security option for the MS Windows operating system that helps prevent hackers from intruding into a computing device."
+  },
+  {
+    term: "Windows Update",
+    definition: "A Windows Update includes the Microsoft Windows operating system patches and security updates. The Automatic Update feature enables MS Windows to connect and download updates automatically."
+  },
+  {
+    term: "Wired Equivalent Privacy (WEP)",
+    definition: "WEP is a networking security protocol replaced by Wi-Fi Protected Access (WPA). WEP is an IEEE standard for wireless networking based on spread-spectrum radio transmission in the 2.4 GHz and 5 GHz bands."
+  },
+  {
+    term: "Wireless",
+    definition: "Wireless network connectivity uses the electromagnetic broadcast spectrum to send and receive data and voice."
+  },
+  {
+    term: "Word Processing Application",
+    definition: "	Word processing applications enable users to create, edit, and save documents. Most word processing applications can format, edit, review, and share documents."
+  },
+  {
+    term: "Workgroup",
+    definition: "A workgroup is a small group of computers that share resources via a peer-to-peer network."
+  }
+);
