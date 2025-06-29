7ea055b28eda26d3b5d6c6978680f8d55e5c2ce0
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:00:50 +1000

    Update data.js

--- data.js
+++ data.js
@@ -4,62 +4,62 @@ const dictionary = [
   //   term: "32-bit versus 64-bit",
   //   definition: "32-bit versus 64-bit refers to the maximum size of an instruction the CPU can process.",
   //   category: ""
-  // }
+  // },
   {
   //   term: "8.3 Filenames",
   //   definition: "The 8.3 filenames file-naming standard began with Microsoft DOS in 1981 and consisted of an eight-character ASCII name followed by a three-character file extension. Modern operating systems support long filenames, but MS Windows can also generate a short filename based on DOS 8.3 naming rules for compatibility with much older applications.",
   //   category: ""
-  // }
+  // },
     {
   //   term: "Access Control List (ACL)",
   //   definition: "The ACL specifies the user accounts and hosts IP addresses that can access a site and the privileges allowed for the account or program accessing the site.",
   //   category: ""
-  // }
+  // },
     {
   //   term: "Access Point (AP)",
   //   definition: "An AP, such as a router, connects wireless devices and a cabled network. Access points that provide Internet connectivity in grocery stores, shops, restaurants, libraries, airports, and medical facilities are often referred to as hotspots.",
   //   category: ""
-  // }
+  // },
       {
   //   term: "Active Partition",
   //   definition: "The primary partition is the active, bootable partition.",
   //   category: ""
-  // }
+  // },
   {
   //   term: "Adapter Card",
   //   definition: "Adapter card circuit boards provide additional video, sound, and communication.",
   //   category: ""
-  // }
+  // },
     {
   //   term: "Android",
   //   definition: "Android is an open-source operating system built for mobile devices developed by the Open Handset Alliance and primarily sponsored by Google.",
   //   category: ""
-  // }
+  // },
       {
   //   term: "Antivirus Software",
   //   definition: "Antivirus software aims to detect and remove virus infections and most malware. Antivirus software identifies malware code (signatures) and detects suspicious behavior (heuristics). Antivirus software requires frequent updating.",
   //   category: ""
-  // }
+  // },
  {
   //   term: "Apple",
   //   definition: "Apple is a company that manufactures desktops, laptops, all-in-ones, tablets, phones, and other related peripheral devices and software. Apple computers use macOS, built on UNIX, and use Apple-native or third-party developer apps built to run on the macOS platform.",
   //   category: ""
-  // }
+  // },
     {
   //   term: "Audio Card",
   //   definition: "Audio cards are adapter cards that enable computers to record and play sound.",
   //   category: ""
-  // }
+  // },
    {
   //   term: "Audio Port",
   //   definition: "Audio ports, usually located at the edge of the motherboard, provide a connection to the cable to support audio in, speaker out, microphone input, and headphones.",
   //   category: ""
-  // }
+  // },
    {
   //   term: "Authentication",
   //   definition: "Authentication is a method for users to prove who they are. Methods include specifying a username and password, smart cards, biometrics, and additional techniques such as two-factor authentication.",
   //   category: ""
-  // }
+  // },
 {
   //   term: "Availability",
   //   definition: "Availability is the balance of implementing security measures that protect resources from loss, damage, or DoS attacks and providing users with methods to recover access to their devices.",
