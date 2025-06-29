ac89e9e2303f4714d98cc62188ccb690c0d1ef96
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:51:35 +1000

    Create r.js

---
+++ r.js
@@ -0,0 +1,47 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Radio Frequency (RF)",
+    definition: "Radio frequency waves at different frequencies and wavelengths can transmit data. Wi-Fi network products typically work at 2.4 GHz or 5 GHz."
+  },
+  {
+    term: "Random Access Memory (RAM)",
+    definition: "RAM is the principal storage space for computer data and program instructions. RAM is described as volatile because it is temporary, meaning the data is only retained while the computer is in use."
+  },
+  {
+    term: "RCA Connector",
+    definition: "An RCA connector has a distinctive collar or ring for various audio/visual (A/V) functions and equipment. The connector is named after its developer (Radio Corporation of America), but the socket is also called a phono plug."
+  },
+  {
+    term: "Recordable CD Drive",
+    definition: "Recordable CD drives are optical drives that enable users to write data to compact discs (CDs)."
+  },
+  {
+    term: "Recycle Bin",
+    definition: "The recycle bin is where files reside after they are removed from their directory locations and before they are deleted entirely from the hard disk."
+  },
+  {
+    term: "Registered Jack (RJ) Connector",
+    definition: "An RJ connector is used for twisted pair cabling. Four-pair network cabling uses the larger RJ-45 connector. Modem/telephone 2-pair cabling uses the RJ-11 connector."
+  },
+  {
+    term: "Registry",
+    definition: "The registry database is the configuration database for MS Windows hardware and software. Sometimes hardware and software configurations can become out of sync or corrupted. An experienced support person can directly edit the registry using various tools. Always back up the registry before editing the file."
+  },
+  {
+    term: "Remote Desktop Protocol (RDP)",
+    definition: "Microsoft's Remote Desktop Protocol (RDP) allows remote connections to an MS Windows machine for Terminal Services. Terminal Services enables authorized users to log onto the computer via a local network, wide area network, or the Internet."
+  },
+  {
+    term: "Removable Media",
+    definition: "Removable media refers to data or media storage that users can disconnect from their computing device. Removable media includes eSATA drives, external SSD drives, memory cards, tapes, CDs, DVDs, and Blu-ray disks."
+  },
+  {
+    term: "Resolution",
+    definition: "A digital image is made up of many thousands of picture elements (pixels). Resolution describes the number of dots an imaging device can use to sample or display the image, measured in pixels per inch (PPI). The higher the resolution, the better the quality. On a digital printer, the resolution is the number of toner or ink dots that the print engine can put on paper (measured in dots per inch [dpi]). Note that sometimes dpi is used interchangeably with PPI to describe scanner or monitor resolution. Still, image pixels and printer dots are not equivalent, as multiple printer dots are required to represent a single image pixel accurately."
+  },
+  {
+    term: "Router",
+    definition: "Routers connect similar and dissimilar networks at network layer three within the Open Systems Interconnection (OSI) model. Routers can access multiple source and destination addresses within packets based on speed, traffic loads, and costs. They can keep track of multiple active paths within a source and destination network. Network administrators can use TCP/IP routers on a local area network to divide the network into logical subnets."
+  }
+);
