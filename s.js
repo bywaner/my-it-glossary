eaf2ff0ac58718c38dc9b8de5a3b5bf7ab2b9730
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:56:43 +1000

    Create s.js

---
+++ s.js
@@ -0,0 +1,75 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Satellite",
+    definition: "Satellite data transmission happens using microwave transmissions, where orbital satellites relay signals between earth-bound receivers and other orbital satellites. Satellite Internet connectivity uses a receiving antenna connected to the PC or network through a DVB-S modem."
+  },
+  {
+    term: "Scanner",
+    definition: "A scanner is a type of photocopier that converts the image of a physical object into an electronic data file. This file may or may not be editable based on the software included with the scanner."
+  },
+   {
+    term: "Secure Digital (SD) Card",
+    definition: "SD cards are removable storage media and one of the first types of available flash memory cards."
+  },
+   {
+    term: "Secure Sockets Layer (SSL)",
+    definition: "SSL, developed by Netscape, is an authentication method for accessing websites and writing data to databases accessed via those websites."
+  },
+   {
+    term: "Serial ATA (SATA) Drive",
+    definition: "SATA hard drives feature a 7-pin data connector and data throughput of 1.5 Gbps, 3 Gbps, and 6 Gbps. External SATA drives attach to a computer using the eSATA interface."
+  },
+   {
+    term: "Server",
+    definition: "A server is a computer located on a network with memory and storage, which provides access to shared resources."
+  },
+   {
+    term: "Service",
+    definition: "MS Windows machines run services to provide functions such as Plug-and-Play, print spooling, and others. Open the Services app to view, configure, start, and stop these functions."
+  },
+   {
+    term: "Service Pack (SP)",
+    definition: "A service pack includes software updates and customer-specific hotfixes released as one installable file. Service packs often add new features to the OS."
+  },
+   {
+    term: "Service Set ID (SSID)",
+    definition: "The SSID is the name for a specific Wireless LAN (WLAN), as you might find in a café. When multiple access points use the same SSID, this is called an Extended (ESSID)."
+  },
+   {
+    term: "Share",
+    definition: "The installation and enablement of MS Windows File and Print Sharing enable the sharing of folders, files, and printers across networks."
+  },
+   {
+    term: "Shortcut",
+    definition: "A shortcut is an icon that contains a URL to open a program or file. Shortcuts are usually located on the desktop or in the Start menu."
+  },
+   {
+    term: "Smartphone",
+    definition: "A smartphone is a mobile device that uses a lightweight operating system and supports cellular phone calls, SMS text messaging, and Wi-Fi access. Smartphones include general-purpose computing functionality, including cameras, Internet messaging apps, productivity apps, etc. Smartphones typically have screen sizes of between 4 and 5.5 inches."
+  },
+   {
+    term: "Start Menu",
+    definition: "The Start menu lists programs and shortcuts that locate and load applications or folders."
+  },
+   {
+    term: "String",
+    definition: "A string data type supports the storage of variable-length series of characters."
+  },
+   {
+    term: "Stylus",
+    definition: "A digitizer, which enables touchscreen-type capabilities, can use a stylus as a pointing device (rather than a finger), often as a drawing tool."
+  },
+   {
+    term: "Switch",
+    definition: "Networks use multiple types of switches. Ethernet (or LAN) switches perform bridge functions to route the data. Advanced switches perform routing at multiple layers: Layer 3 (IP), 4 (TCP), or 7 (Application). Switches routing at layer 4 or 7 are called load balancers and content switches."
+  },
+   {
+    term: "Synchronous DRAM (SDRAM)",
+    definition: "Synchronous DRAM, an improvement on the basic DRAM chip, is designed to run at the system clock speed, thus accelerating the periodic refresh cycle times."
+  },
+   {
+    term: "System Requirement",
+    definition: "System requirements include the hardware needed to install additional add-on hardware components, operating systems, application software, and utility software."
+  }
+);
