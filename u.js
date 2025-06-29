0f44c13cfe4a876f42d6e1d8e074ed8feb560083
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 19:02:10 +1000

    Create u.js

---
+++ u.js
@@ -0,0 +1,35 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Unicode",
+    definition: "Unicode comprises code points and includes code point and hexadecimal characters that represent character glyphs. Unicode supports international alphabets."
+  },
+  {
+    term: "Universal Disk Format (UDF)",
+    definition: "UDF is the file system used for optical media, replacing the Compact Disk File System (CDFS)."
+  },
+  {
+    term: "Universal Serial Bus (USB)",
+    definition: "A USB is a device used to connect computers with peripheral devices, such as mice, keyboards, printers, etc."
+  },
+  {
+    term: "UNIX System",
+    definition: "AT&T developed the UNIX operating system and first used it in 1969. Considered a general-purpose operating system, UNIX was novel at the time for its support for parallel processing and multiple vendor hardware platforms. UNIX servers are used for the Internet backbone and use TCP/IP communication protocols for network compatibility."
+  },
+  {
+    term: "Update",
+    definition: "Software and hardware manufacturers push firmware and software updates to fix malfunctions, address security vulnerabilities, and enhance features. Updates include hotfixes (customer-specific fixes), patches (available fixes), and service packs (combined patches and enhancements)."
+  },
+  {
+    term: "User Account",
+    definition: "A user account is an individual account within MS Windows User Account Control."
+  },
+  {
+    term: "User Account Control (UAC)",
+    definition: "This MS Windows security system enables user privilege based on need so that users don't inadvertently or intentionally make changes that would harm the computer, the organization, or both."
+  },
+  {
+    term: "User Datagram Protocol (UDP)",
+    definition: "UDP works in the network transport layer to provide fast communications. This protocol is faster than TCP but does not ensure reliability."
+  }
+);
