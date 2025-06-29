11dc3322b4c449079d93ccda7a9d726d94a498ad
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 15:36:43 +1000

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+const dictionary = [
+  // Add your vocabulary like this:
+  // {
+  //   term: "API",
+  //   definition: "A set of rules that let software components communicate.",
+  //   category: "Programming"
+  // }
+];
