ec345673a379bd39bfca3067f989cf11ef97af64
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:23:53 +1000

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,7 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "32-bit versus 64-bitI",
+    definition: "32-bit versus 64-bit refers to the maximum size of an instruction the CPU can process."
+  }
+);
