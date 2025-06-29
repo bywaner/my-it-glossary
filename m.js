c24cae7de70513b008d34e6d5f780f1a76cf9ac7
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:20:43 +1000

    Create m.js

---
+++ m.js
@@ -0,0 +1,43 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Mapping",
+    definition: "Mapping is establishing a connection with a file resource on a remote server, for example, mapping a network drive identified by a letter or name."
+  },
+  {
+    term: "Microsegmentation",
+    definition: "In a switched network, microsegmentation is the data routing technique that keeps each port in a separate collision domain, preventing data collisions."
+  },
+  {
+    term: "Microsoft",
+    definition: "Microsoft is the world's foremost supplier of operating systems and productivity software. Since developing the first IBM-compatible PCs running MS-DOS, Microsoft has dominated the PC market."
+  },
+  {
+    term: "Mobile Device",
+    definition: "Mobile devices are general-purpose computing devices, such as mobile phones, phablets, and tablets, that humans can carry to multiple locations. These devices often store valuable personal or corporate information."
+  },
+  {
+    term: "Mobile Phone",
+    definition: "A mobile phone is the UK English term for a cell phone."
+  },
+  {
+    term: "Modem (Modulator/Demodulator)",
+    definition: "Modems convert the digital signals from a computer into the analog signal required to transmit data over public phone lines, a process known as modulation. Demodulation occurs at the receiving computer."
+  },
+  {
+    term: "Motherboard",
+    definition: "The computer motherboard, also called the system board, provides the foundation for all the computer's hardware, including the processor, RAM, BIOS, and expansion cards. Several motherboard standards are available, each with a different layout and associated advantages."
+  },
+  {
+    term: "Mouse",
+    definition: "A mouse is an input device that interfaces with graphical displays. With a mouse, a user can control the movement of a cursor to select on-screen objects."
+  },
+  {
+    term: "Multimedia",
+    definition: "Multimedia refers to PC components that can input and output sound and video."
+  },
+  {
+    term: "Multiprocessing",
+    definition: "Multiprocessing happens when two or more processors are used on a single motherboard. Both processors can share operations, thereby increasing performance. Business and professional editions of MS Windows support a type of multiprocessing called Symmetric Multiprocessing (SMP) with a maximum of 2 CPUs."
+  }
+);
