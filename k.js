eba91f88641530692f723ee6d7f526d44407363e
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:14:07 +1000

    Create k.js

---
+++ k.js
@@ -0,0 +1,15 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Kernel",
+    definition: "A kernel is a low-level piece of core code in an operating system that controls the rest of the operating system. MS Windows uses a multiprocessor-aware, preemptive, multitasking kernel."
+  },
+  {
+    term: "Keyboard",
+    definition: "A keyboard is the oldest PC input device still fundamental to operating a computer. Desktop keyboards can have PS/2, USB, or wireless (IrDA or Bluetooth) interfaces. There are many designs and layouts for different countries. Some keyboards feature special keys."
+  },
+ {
+    term: "Knowledge Base",
+    definition: "A knowledge base is a searchable database of advice, frequently asked questions and solutions to known troubleshooting issues."
+  }
+);
