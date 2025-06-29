77769855846f4fb71e1f7e90dde03c0974dce790
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:23:46 +1000

    Create o.js

---
+++ o.js
@@ -0,0 +1,11 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Open Source",
+    definition: "	Open source refers to programming code that is freely available. Linux is an example of open-source operating system code. Learn more at opensource.org."
+  },
+  {
+    term: "OS X",
+    definition: "Based on the BSD version of UNIX, OS X is Apple's operating system for iMac computers, Mac workstations, and MacBook portables."
+  }
+);
