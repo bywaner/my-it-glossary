23de1615b2dfe5f9d8ee302ed1f67a6e5192dd3b
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:03:46 +1000

    Create f.js

---
+++ f.js
@@ -0,0 +1,59 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Facsimile (Fax)",
+    definition: "A facsimile is the transfer of the image of a document over a telephone line using a fax modem that is usually part of a single fax machine, a multifunction printer, or within PC equipment."
+  },
+  {
+    term: "Fault Tolerance (Redundancy)",
+    definition: "Fault-tolerant systems provide extra capacity, or redundancy, which helps organizations identify and eliminate causes of failure."
+  },
+   {
+    term: "File",
+    definition: "A file is a collection of data. Files store data either as plain text data or binary data. Files have a descriptive name followed by a period and a three-character extension, which is used to associate the file with a particular software application."
+  },
+   {
+    term: "File Allocation Table (FAT)",
+    definition: "FAT is a method of formatting a hard disk. The FAT contains information that describes the position of file data chunks on the disk. Data for a single file can exist on multiple tracks."
+  },
+   {
+    term: "File Server",
+    definition: "A file server is a network-based machine that provides centralized data and print services access."
+  },
+   {
+    term: "File System",
+    definition: "When data is stored on a disk, the data is stored in a file system based on the directory and file sizes available based on how the disk was partitioned. File systems include FAT16, FAT32, and NTFS (used for internal storage) and optical storage formats, including CDFS (ISO 9660) and UDF (Universal Disk Format)."
+  },
+   {
+    term: "Firewall",
+    definition: "A firewall consists of hardware, software, or a combination of both that examines the network packet data passing through the network and determines if the data should be allowed past the firewall to the destination computing devices."
+  },
+   {
+    term: "Firewire (IEEE 1394 Standard)",
+    definition: "The IEEE standard 1394 port, called a Firewire, is a serial SCSI bus standard that supports up to 400 Mbps data transfer rates. Firewire enables intensive data transfer, such as digital media transfers."
+  },
+   {
+    term: "Firmware",
+    definition: "Firmware comprises software instructions stored semi-permanently (embedded) on a hardware device."
+  },
+   {
+    term: "Float",
+    definition: "Float is the data type supporting the storage of floating-point numbers (decimal fractions)."
+  },
+   {
+    term: "Frame",
+    definition: "Just like packets, frames are small parts of a message within the network that help decode and interpret the data, including source and target MAC (hardware) addresses and the data and error-checking regions. The main difference between a packet and a frame is the association with the network's OSI data link layer. Frames contain more information about the transmitted message than a packet."
+  },
+   {
+    term: "Frontside Bus",
+    definition: "The frontside bus is between the CPU and the memory controller (system RAM)."
+  },
+   {
+    term: "Function (Fn) Key",
+    definition: "Function keys are special keys or key combinations that access the BIOS and adjust computing settings such as volume up and down, microphone off and on, screen brightness, and others."
+  },
+   {
+    term: "Fuser",
+    definition: "A fuser is the assembly that fixes toner to media using a combination of a heat and pressure roller."
+  }
+);
