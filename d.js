e39aab5401d2b5e35712d630f1beeb3fc68f77c5
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 17:45:45 +1000

    Create d.js

---
+++ d.js
@@ -0,0 +1,71 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Data Rate",
+    definition: "The data rate is the speed at which data is sent and received among two or more computer components."
+  },
+  {
+    term: "Data Unit",
+    definition: "A data unit is a fundamental unit of data storage. A bit is a computer's most fundamental data unit and computer storage."
+  },
+   {
+    term: "Default Account",
+    definition: "Default accounts include default administrative and guest accounts configured on servers and networks. A good practice is to rename the MS Windows administrative account. Another good practice is to leave the "root" system owner account unused on UNIX/Linux systems."
+  },
+   {
+    term: "Default Gateway",
+    definition: "The default gateway is a TCP/IP address parameter that identifies the location of a router on the local subnet. The host device uses that TCP/IP address as part of the routing information needed to contact other networks."
+  },
+   {
+    term: "Desktop",
+    definition: "The desktop is the main window displayed when a user opens a computer. The term "desktop" is usually associated with Microsoft Windows computers."
+  },
+   {
+    term: "Device Driver",
+    definition: "Device driver code, usually provided by the hardware manufacturer, loads on startup and works with the OS to establish communication with the additional hardware. MS Windows uses a signing system for drivers to validate that the code is approved."
+  },
+   {
+    term: "Device Manager",
+    definition: "The MS Windows Device Manager enables the administrator to add devices, view device properties, disable and remove devices, update device drivers, view system resources, and update device drivers."
+  },
+   {
+    term: "Digital Camera (Digicam)",
+    definition: "Digital cameras use light-sensitive diodes and nonvolatile electronic storage media. The array's sensitivity determines the image's maximum resolution, usually documented in pixels. Digital cameras can connect to a computer using a USB or Firewire port. However, with digital cameras as part of smartphones, users often store images on the cloud and download images to other sites for publication and storage."
+  },
+   { 
+    term: "Digital Signaling",
+    definition: "Digital signaling uses discrete states to represent simple values, such as 1 or 0. "
+  },
+   { 
+    term: "Digital Video Interface (DVI)",
+    definition: "DVI is a video adapter designed to replace VGA ports on CRT monitors. DVI interfaces can support digital-only or digital and analog signaling."
+  },
+   {
+    term: "Directory",
+    definition: "A directory is a file system object that organizes files within a specific space. Directories, also known as folders, are created within the hard drive. The main space that holds directories and subdirectories is called the root folder."
+  },
+   {
+    term: "Disaster Recovery Plan",
+    definition: "The disaster recovery plan, in conjunction with business continuity, sets out actions and responsibilities for foreseen and unforeseen critical incidents. Disaster recovery plans include the actions and responsibilities in response to critical incidents. Recovery plans can also include practice exercises to assess disaster recovery procedures."
+  },
+   {
+    term: "Disk Operating System (DOS)",
+    definition: "Widely adopted in the 1980s, Microsoft developed MS-DOS as a single-task operating system. The last MS-DOS release was version 6.22, released in June 1994."
+  },
+  {
+    term: "DisplayPort",
+    definition: "DisplayPort, developed by Video Electronics Standards Association (VESA), is a digital A/V interface that supports some cross-compatibility with DVI and HDMI devices."
+  },
+  {
+    term: "Domain Name System (DNS)",
+    definition: "The DNS is a hierarchical, distributed database that provides a worldwide name to IP address mapping services for the Internet and large intranets."
+  },
+  {
+    term: "Dual Core",
+    definition: "Dual core is a CPU design that puts two chips onto the same platform."
+  },
+  {
+    term: "Dynamic RAM (DRAM)",
+    definition: "DRAM is a type of volatile memory that stores data in the form of electronic charges within transistors. Standard DRAM is the most basic type of DRAM. Modern PCs usually use upgraded DRAM to help process data."
+  }
+);
