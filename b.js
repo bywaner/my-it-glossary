e3be90aa82381f8711cb22906e4a63c4cbbf3045
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:38:25 +1000

    Create b.js

---
+++ b.js
@@ -0,0 +1,55 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Backup",
+    definition: "A backup is a copy of data stored for use in case of a system failure. Backups help prevent loss of work."
+  },
+  {
+    term: "Battery",
+    definition: "A battery is a power source for computing devices, typically rechargeable Lithium-ion (Li-ion) batteries of differing capacities. Computers use coin cell batteries to power their CMOS RAM."
+  },
+  {
+    term: "Binary",
+    definition: "The binary is a notational system with two values per digit, zero and one."
+  },
+  {
+    term: "Biometric",
+    definition: "Biometric identification is physically identifying features as security authentication to access a device, software application, or website."
+  },
+  {
+    term: "Bit",
+    definition: "A bit, the smallest measurable computer value is a single digit with two values, zero and one."
+  },
+  {
+    term: "Bits per Second (bps)",
+    definition: "Bits per second is a data rate measurement used to describe data transfer speeds."
+  },
+  {
+    term: "Bluetooth",
+    definition: "Bluetooth is a short-range, radio-based technology that works up to 10 m (30 feet) at up to 1 Mbps connecting computing devices with peripherals."
+  },
+  {
+    term: "Blu-ray",
+    definition: "Blu-ray is optical drive technology with a disc capacity of 25 GB per layer. Transfer rates are measured in multiples of 36 MBps."
+  },
+  {
+    term: "Boolean",
+    definition: "Boolean is a data type that supports 1-bit storage, representing true and false values. Boolean logic is a statement that uses true and false conditions. Boolean logic supports the branching and looping features in computer code."
+  },
+  {
+    term: "Boot Partition",
+    definition: "The boot partition, or system partition, for both Apple and MS Windows devices, is the partitioned part of the hard drive that contains the MS Windows operating system."
+  },
+  {
+    term: "Browser",
+    definition: "A web browser is software designed to view HTML pages that runs plug-in applications to enable file formats, including PDFs, sound, and video. "
+  },
+  {
+    term: "Bus",
+    definition: "A physical connection between motherboard components and peripheral devices attached to the computer. A bus's capabilities include data sharing, memory addressing, and supplying power to the port. Common bus types include PCI, PCI Express, ExpressCard, and USB."
+  },
+  {
+    term: "Byte",
+    definition: "A byte is the value of eight bits."
+  }
+);
