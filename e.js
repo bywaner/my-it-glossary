32a256d7bc3ece1d9dc05b431af7b630e2e7cd62
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 17:59:18 +1000

    Create e.js

---
+++ e.js
@@ -0,0 +1,15 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Embedded System",
+    definition: "Embedded systems contain proprietary operating systems and perform specific, dedicated functions. Appliances, spacecraft, and IoT devices use embedded systems."
+  },
+  {
+    term: "Encryption",
+    definition: "Encryption is the masking and/or scrambling of characters to securely store data, including passwords and messages.."
+  },
+  {
+    term: "Ethernet (802.3)",
+    definition: "Ethernet (802.3) is a popular local area networking technology method. Ethernet is compatible with coax, thinnet, UTP, and fiber optic cables. Wireless devices can also connect to Ethernet networks via a Wireless Access Point."
+  }
+);
