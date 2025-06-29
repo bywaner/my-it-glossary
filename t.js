5a3dd183cc4a2dc134fa9108d2039922c29bdd32
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:59:55 +1000

    Create t.js

---
+++ t.js
@@ -0,0 +1,51 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Tablet",
+    definition: "A tablet is a type of ultra-portable laptop with a touchscreen. Tablets run specialized operating systems, including Android, iOS, MS Windows, and ChromeOS."
+  },
+  {
+    term: "Task Manager",
+    definition: "The MS Windows Task Manager app enables the control of running tasks, processes, and CPU/memory utilization."
+  },
+  {
+    term: "Task Scheduler",
+    definition: "The Task Scheduler enables users to set tasks such as program starts and stops to happen at a preset time or in response to another event."
+  },
+  {
+    term: "Taskbar",
+    definition: "The Taskbar is a means of locating running programs and contains the Start menu, system tray/notification area (as well as an optional Quick Launch toolbar). It appears by default at the bottom of the desktop."
+  },
+  {
+    term: "Test Page",
+    definition: "A test page is a printed page generated after installing or updating a printer that verifies the printer is working correctly."
+  },
+  {
+    term: "Thumb Drive",
+    definition: "A thumb drive is a flash memory card contained within a case. Thumb drives connect to computing devices using a USB adapter."
+  },
+  {
+    term: "Thunderbolt",
+    definition: "The Thunderbolt (TB) interface was developed by Intel and until recently was primarily used on Apple workstations and laptops. Thunderbolt adapters can power display interfaces (like DisplayPort) and function as a USB3 port."
+  },
+  {
+    term: "Toner",
+    definition: "Toner is a chemical that uses dye, wax, or plastic and uses an electromagnetic charge to function as ink to print to paper or other materials."
+  },
+  {
+    term: "Touchpad",
+    definition: "A touchpad is an input device used on most laptops to replace the mouse. The touchpad lets the user control the cursor by moving a finger over the pad's surface. Touchpads can have buttons, recognize "tap" events, and perform scrolling functions."
+  },
+  {
+    term: "Touchscreen",
+    definition: "Touchscreens display output and accept programming that enables touch user input."
+  },
+  {
+    term: "Transfer Rate",
+    definition: "The transfer rate is the data sent or received via a network connection in a specific time, typically measured in bits or bytes per second."
+  },
+  {
+    term: "Troubleshooting",
+    definition: "Troubleshooting is the methodical approach to diagnosing and resolving issues that affect computer performance."
+  }
+);
