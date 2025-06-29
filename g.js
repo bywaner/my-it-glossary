635fc0bdc18c7a5c7c4091f5fa71a542547a739b
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:08:00 +1000

    Update g.js

--- g.js
+++ g.js
@@ -11,29 +11,5 @@ window.dictionary.push(
   {
     term: "Group Account",
     definition: "A group account is a collection of user accounts."
-  },
-  {
-    term: "Hard Disk Drive (HDD)",
-    definition: "Hard disk drives provide nonvolatile, long-term data storage. An HDD installed within a PC is known as a hard disk. Portable, external, enclosed hard disk data storage is also known as Network Attached Storage (NAS)."
-  },
-  {
-    term: "Hexadecimal",
-    definition: "Hexadecimal, known as hex, is a notational system with 16 values per digit. Values above nine are represented by the letters A, B, C, D, E, and F. Hex is a compact way of referring to long byte values, MAC and IPv6 addresses, and color notations for web pages."
-  },
-  {
-    term: "Homegroup",
-    definition: "Used with MS Windows 7, Homegroup is a networking feature that enables MS Windows 7 home networks to share files and printers easily via passwords."
-  },
-  {
-    term: "Host",
-    definition: "In TCP/IP networking terminology, a host is any device that connects to a network."
-  },
-  {
-    term: "Hot Swappable",
-    definition: "A hot-swappable device can be added or removed without restarting the operating system."
-  },
-  {
-    term: "Hybrid Fiber Coax",
-    definition: "Hybrid fiber coax cables, which include a fiber optic line and coax cable, are usually installed by a cable provider to carry television and Internet access within the same cable."
   }
 );
