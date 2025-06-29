387bf2c5091621c28a794beabae35083f8f4683c
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:32:07 +1000

    Create a.js

---
+++ a.js
@@ -0,0 +1,47 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Access Control List (ACL)",
+    definition: "The ACL specifies the user accounts and hosts IP addresses that can access a site and the privileges allowed for the account or program accessing the site."
+  },
+  {
+    term: "Access Point (AP)",
+    definition: "An AP, such as a router, connects wireless devices and a cabled network. Access points that provide Internet connectivity in grocery stores, shops, restaurants, libraries, airports, and medical facilities are often referred to as hotspots."
+  },
+  {
+    term: "Active Partition",
+    definition: "The primary partition is the active, bootable partition.."
+  },
+   {
+    term: "Adapter Card",
+    definition: "Adapter card circuit boards provide additional video, sound, and communication."
+  },
+   {
+    term: "Android",
+    definition: "Android is an open-source operating system built for mobile devices developed by the Open Handset Alliance and primarily sponsored by Google."
+  },
+   {
+    term: "Antivirus Software",
+    definition: "Antivirus software aims to detect and remove virus infections and most malware. Antivirus software identifies malware code (signatures) and detects suspicious behavior (heuristics). Antivirus software requires frequent updating."
+  },
+   {
+    term: "Apple",
+    definition: "Apple is a company that manufactures desktops, laptops, all-in-ones, tablets, phones, and other related peripheral devices and software. Apple computers use macOS, built on UNIX, and use Apple-native or third-party developer apps built to run on the macOS platform."
+  },
+   {
+    term: "Audio Card",
+    definition: "Audio cards are adapter cards that enable computers to record and play sound."
+  },
+   {
+    term: "Audio Port",
+    definition: "Audio ports, usually located at the edge of the motherboard, provide a connection to the cable to support audio in, speaker out, microphone input, and headphones."
+  },
+   {
+    term: "Authentication",
+    definition: "Authentication is a method for users to prove who they are. Methods include specifying a username and password, smart cards, biometrics, and additional techniques such as two-factor authentication.."
+  },
+   {
+    term: "Availability",
+    definition: "Availability is the balance of implementing security measures that protect resources from loss, damage, or DoS attacks and providing users with methods to recover access to their devices."
+  }
+);
