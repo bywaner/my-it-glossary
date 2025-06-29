8d7d483f5699a5d550b884b5f3c70d9d86f7f027
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:07:15 +1000

    Create g.js

---
+++ g.js
@@ -0,0 +1,39 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Gesture-based Interaction",
+    definition: "Gesture-based interaction enables swipe, pinch, and stretch actions to perform tasks using a touchscreen."
+  },
+  {
+    term: "Graphical User Interface (GUI)",
+    definition: "A graphical user interface, or GUI (pronounced "gooey"), displays text and images on a monitor. Most GUIs require an input pointing device, such as a mouse or keyboard, to operate efficiently and an output device, such as a monitor. Although these operating systems have command line interfaces, MS Windows, macOS, Chrome OS, iOS, and Android operating systems, by default, are GUI operating systems."
+  },
+  {
+    term: "Group Account",
+    definition: "A group account is a collection of user accounts."
+  },
+  {
+    term: "Hard Disk Drive (HDD)",
+    definition: "Hard disk drives provide nonvolatile, long-term data storage. An HDD installed within a PC is known as a hard disk. Portable, external, enclosed hard disk data storage is also known as Network Attached Storage (NAS)."
+  },
+  {
+    term: "Hexadecimal",
+    definition: "Hexadecimal, known as hex, is a notational system with 16 values per digit. Values above nine are represented by the letters A, B, C, D, E, and F. Hex is a compact way of referring to long byte values, MAC and IPv6 addresses, and color notations for web pages."
+  },
+  {
+    term: "Homegroup",
+    definition: "Used with MS Windows 7, Homegroup is a networking feature that enables MS Windows 7 home networks to share files and printers easily via passwords."
+  },
+  {
+    term: "Host",
+    definition: "In TCP/IP networking terminology, a host is any device that connects to a network."
+  },
+  {
+    term: "Hot Swappable",
+    definition: "A hot-swappable device can be added or removed without restarting the operating system."
+  },
+  {
+    term: "Hybrid Fiber Coax",
+    definition: "Hybrid fiber coax cables, which include a fiber optic line and coax cable, are usually installed by a cable provider to carry television and Internet access within the same cable."
+  }
+);
