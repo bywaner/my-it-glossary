89dc1f5e7439849be69653d8c52ad8abfb40a643
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:24:48 +1000

    Create 8.js

---
+++ 8.js
@@ -0,0 +1,7 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "8.3 Filenames",
+    definition: "The 8.3 filenames file-naming standard began with Microsoft DOS in 1981 and consisted of an eight-character ASCII name followed by a three-character file extension. Modern operating systems support long filenames, but MS Windows can also generate a short filename based on DOS 8.3 naming rules for compatibility with much older applications."
+  }
+);
