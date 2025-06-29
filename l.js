59f715fd01557b2073895657467cf65d9bd7c5f7
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:17:34 +1000

    Create l.js

---
+++ l.js
@@ -0,0 +1,47 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Laptop/Notebook",
+    definition: "A laptop/notebook is a powerful portable computer offering similar functionality to a desktop computer. Laptops include built-in output and input devices (monitor, keyboard, and touchpad) powered by AC and DC power. These devices typically include USB, PCMCIA, ExpressCard adapters, and Bluetooth adapters for connecting peripherals."
+  },
+  {
+    term: "Laser Printer",
+    definition: "A laser printer develops an image on a drum using electrical charges to attract special toner in one or more colors. The toner is then fixed to the paper using a high-heat and pressure roller (fuser)."
+  },
+   {
+    term: "Least Privilege",
+    definition: "Least privilege is a basic principle of security stating that a user is given the minimum necessary rights, privileges, or information to perform their role."
+  },
+   {
+    term: "Legacy Device",
+    definition: "Legacy devices are hardware, such as printers and scanners, which are not Plug-and-Play compatible and require additional configuration."
+  },
+   {
+    term: "Library",
+    definition: "Virtual folders, called libraries, introduced in MS Windows 7, are predefined locations for documents, pictures, and music."
+  },
+   {
+    term: "Licensing",
+    definition: "Licensing is the contracted terms and conditions users accept to install and use an operating system or other software on a single computer or multiple computers."
+  },
+   {
+    term: "Lightning Connector",
+    definition: "A lightning connector is a proprietary connector and interface for Apple devices."
+  },
+   {
+    term: "Linux",
+    definition: "Linux is an open-source operating system developed by Linus Torvalds for enterprise and personal use."
+  },
+   {
+    term: "Liquid Cooling System",
+    definition: "A liquid cooling system uses water piped around the PC board components and heatsinks to cool those components. Liquid cooling systems allow for fewer fans and less noise."
+  },
+   {
+    term: "Local Area Network (LAN)",
+    definition: "A LAN is a network owned/managed by a single organization in one geographic location."
+  },
+   {
+    term: "Long File Name",
+    definition: "A long file name is a 255-character Unicode file name that can contain spaces and multiple periods."
+  }
+);
