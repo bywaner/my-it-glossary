846c84998718f8ea06eb78b7e7df71bce3d89194
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:11:58 +1000

    Create i.js

---
+++ i.js
@@ -0,0 +1,43 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Inkjet Printer",
+    definition: "An inkjet printer, also known as an ink dispersion printer, is a type of printer that uses nozzles to spray colors of ink onto paper."
+  },
+  {
+    term: "Input/Output (I/O) Port",
+    definition: "An I/O port is a device connection to send and receive data."
+  },
+   {
+    term: "Integer",
+    definition: "An integer is a whole number and is usually a supported data type."
+  },
+   {
+    term: "Intel",
+    definition: "Intel processors were used in the first IBM PCs, and the company's CPUs and chip sets continue to dominate the PC and laptop market."
+  },
+   {
+    term: "Internet",
+    definition: "The Internet core consists of high-speed data communications lines using TCP/IP protocols by major host computers, hosted by thousands of commercial, government, educational, and other computer systems that route data and messages. No single company or organization owns the Internet."
+  },
+   {
+    term: "Internet of Things (IoT)",
+    definition: "IoT devices are special-purpose computers, usually with a specialized operating system, designed to perform limited, specific functions."
+  },
+   {
+    term: "Internet Protocol (IP)",
+    definition: "IP is the network-layer protocol in TCP/IP that enables packet addressing and routing for all higher-level protocols in the suite."
+  },
+   {
+    term: "Internet Protocol (IP) Address",
+    definition: "Each receiving computer must have a manually or dynamically allocated IP address."
+  },
+   {
+    term: "Intranet",
+    definition: "An intranet is a company's network."
+  },
+   {
+    term: "iOS",
+    definition: "iOS is the mobile operating system developed by Apple for its iPhone and iPad devices."
+  }
+);
