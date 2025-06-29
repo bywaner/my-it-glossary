921c6a32ef13294a5963e0103afb1f96e0583d21
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 18:22:26 +1000

    Create n.js

---
+++ n.js
@@ -0,0 +1,23 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Near Field Communication (NFC)",
+    definition: "Based on RFID technology, NFC is a standard communication type for peer-to-peer radio communications over approximately 1.5 inches. NFC enables the exchange of contact information, contactless payments, and others."
+  },
+  {
+    term: "Network",
+    definition: "Networks are two or more computers that can connect to share data."
+  },
+  {
+    term: "Network Address",
+    definition: "Like having a home address, computing devices require a network address for other computing devices to find them. The IPv4 address method uses a 32-bit binary number, typically expressed as a 4-part decimal number separated by dots, while IPv6 uses a 128-bit binary number displayed using hexadecimal formatting. Networks, network devices, and peripherals on the network also have their own addresses."
+  },
+  {
+    term: "Network Attached Storage (NAS)",
+    definition: "NAS is storage hardware with an embedded OS that enables fast \"near\" computer access. Users should take extra care to protect NAS storage from security concerns."
+  },
+  {
+    term: "Notification Area",
+    definition: "The notification area is part of the taskbar (usually on the right-hand side) that displays background applications and status information (such as the date and time, antivirus software, network connections, and alerts). Some users may refer to this space as the system tray."
+  }
+);
