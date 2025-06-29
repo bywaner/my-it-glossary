ec21d42aa570093af615f492492aa3e126e2dcfc
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 16:57:35 +1000

    Update c.js

--- c.js
+++ c.js
@@ -8,51 +8,51 @@ window.dictionary.push(
     term: "Celeron Processor Series",
     definition: "The Celeron processor series is a set of CPU models produced by Intel alongside their Pentium and Core brands."
   },
-   {
+  {
     term: "Cell Phone",
     definition: "Cell phones are mobile devices that send and receive data and voice communications using base station transmitters, known as cells, which connect to cellular and telephone networks. Data communications are divided into bands with differing data transmission capabilities: 4G (LTE, up to about 150 Mbps) and 5G (up to 1 Gbps). Most cellular networks in the United States now offer 4G and 5G communications."
   },
-   {
+  {
     term: "Central Processing Unit (CPU)",
     definition: "The CPU is the principal microprocessor in a computer or smartphone responsible for running operating systems and applications software."
   },
-   {
+  {
     term: "Char",
     definition: "Char, short for character, is a data type representing one data storage unit. ASCII and Unicode are char data types."
   },
-   {
+  {
     term: "Chip Set",
-    definition: "A chip set communicates with components using controllers. Chip sets can also include "integrated" adapters for video, sound, and network connections. Chip sets placed close to the CPU are referred to as northbridge chip sets. Chip sets further away on the board, or located at the perimeter of the board, are referred to as southbridge."
+    definition: "A chip set communicates with components using controllers. Chip sets can also include \"integrated\" adapters for video, sound, and network connections. Chip sets placed close to the CPU are referred to as northbridge chip sets. Chip sets further away on the board, or located at the perimeter of the board, are referred to as southbridge."
   },
-   {
+  {
     term: "Chrome OS",
     definition: "Based on Linux, Chrome OS is a proprietary operating system developed by Google to run on specific laptops (Chromebooks) and personal computers (Chromebox)."
   },
-   {
+  {
     term: "Client",
     definition: "A client device is any computer hardware or software device that requests access to a service, such as a printer or stored data, which is available via the network."
   },
-   {
+  {
     term: "Compatibility Mode",
     definition: "Compatibility mode is the capability of an application to work with data, files, and settings from previous versions of an application or operating system."
   },
-   {
+  {
     term: "Computer Management Console",
     definition: "The Computer Management Console provides access to Windows Device Manager, Event Viewer, Disk Management, Services, and Performance Monitor. One way to access the console is to type Computer Management in the Taskbar search field and open the Computer Management app."
   },
-   {
+  {
     term: "Control Panel",
     definition: "The control panel is the primary MS Windows management interface for managing operating system settings."
   },
-   {
+  {
     term: "Converged Network",
     definition: "Converged networks carry both voice and data. These networks require additional security considerations to mitigate the vulnerability to denial of services (DoS) and eavesdropping."
   },
-   {
+  {
     term: "Cooling Device",
     definition: "Cooling devices dissipate the heat that CPUs generate. This heat can damage the processor or chip. Generally, CPUs include a heatsink consisting of a metal block with fins and a fan. A thermal compound applied at the contact point between the chip and the heatsink helps heat transfer. Most computers also include one or more fans that route heated air out of the case. Modern motherboards have temperature sensors that warn of overheating before damage can occur. Large computing systems and performance PCs may require liquid cooling. Active cooling systems use electricity. Passive cooling systems do not require electricity."
   },
-   {
+  {
     term: "Core Processor Series",
     definition: "Core Processor Series is an Intel processor brand. Core, Core 2, and Core iX CPUs are based on the Pentium M architecture."
   }
