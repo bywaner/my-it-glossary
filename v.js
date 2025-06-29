6ee3a561053da67e861dbe8b4ce48de15c1ccb2c
waner-research <110174120+bywaner@users.noreply.github.com>
2025-06-29 19:04:36 +1000

    Create v.js

---
+++ v.js
@@ -0,0 +1,19 @@
+window.dictionary = window.dictionary || [];
+window.dictionary.push(
+  {
+    term: "Video Card",
+    definition: "Video cards, also known as graphics adapters, provide the interface between the graphics components of the computer and display devices. Video cards can work with VGA-, DVI-, and HDM-connected internal and external displays. Most video cards include a processor (Graphics Processing Unit [GPU]) and onboard memory."
+  },
+  {
+    term: "Video Conferencing",
+    definition: "Video conferencing or video teleconferencing (VTC) software allows users to use their input and output devices to conduct voice and video meetings. Some software packages include options for multiple individual meeting rooms and instant messaging."
+  },
+  {
+    term: "Video Standard",
+    definition: "Video standards define the resolution and color depth for a graphics adapter and computer display. You can find the available video standards within the operating systems settings."
+  },
+  {
+    term: "Virtual Memory",
+    definition: "Virtual memory (also known as swapping or paging) is an area on the hard disk allocated for volatile memory when the installed RAM is insufficient or the hardware is malfunctioning."
+  }
+);
